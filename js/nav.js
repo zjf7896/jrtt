4a7e1bfb6b30a9c8e67d86d7e90ac3de548c3713
tark S <344754617@qq.com>
2018-09-03 15:51:30 +0800

    1st

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+var del=document.querySelectorAll('.del-box .content .list');
+var dels=document.querySelector('.del-box .content');
+console.log(del);
+var add=document.querySelectorAll('.add-box .content .list');
+var adds=document.querySelector('.add-box .content');
+console.log(add);
+add.forEach(
+    add[i].onclick=function () {
+       this.
+    }
+);
